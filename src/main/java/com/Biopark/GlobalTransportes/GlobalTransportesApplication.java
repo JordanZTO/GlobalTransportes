00d66823b90981143a797f426b14eb5e558d4c9f
jordanZTO <jordanmunchen@gmail.com>
2025-05-26 20:19:33 -0300

    Estrutura inicial ja com dependencias necessarias

---
+++ src/main/java/com/Biopark/GlobalTransportes/GlobalTransportesApplication.java
@@ -0,0 +1,13 @@
+package com.Biopark.GlobalTransportes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlobalTransportesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlobalTransportesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Biopark/GlobalTransportes/GlobalTransportesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Biopark.GlobalTransportes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlobalTransportesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
